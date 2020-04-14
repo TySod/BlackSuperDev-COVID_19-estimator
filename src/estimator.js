823d8c9daf34dcad58aea1b8cb64a15c11694261
TySod <36824700+TySod@users.noreply.github.com>
2020-04-14 04:14:17 +0100

    challenge 2d2

--- src/estimator.js
+++ src/estimator.js
@@ -52,7 +52,7 @@ const covid19ImpactEstimator = (data) => {
 
   const eRequestedTime = periodCheck(dur);
   const eSCasesByRequestedTime = (0.15 * periodCheck(dur));
-  const eHBedsByRequestedTime = Math.trunc(beds - (0.15 * periodCheck(dur)));
+  const eHBedsByRequestedTime = Math.trunc((0.15 * periodCheck(dur)) - (beds * 0.35));
   const eC4ICUByRequestedTime = (periodCheck(dur) * 0.05);
   const eC4VentilatorsByRequestedTime = (periodCheck(dur) * 0.02);
   const eDollarsInFlight = ((periodCheck(dur) * percentIncome * avgInc)) / timeEst(dur);
@@ -68,8 +68,8 @@ const covid19ImpactEstimator = (data) => {
 
 
   const pRequestedTime = severeCalc(dur);
-  const pSCByRequestedTime = severeCalc(dur);
-  const pHBedsByRequestedTime = Math.trunc(beds - (severeCalc(dur) * 0.15));
+  const pSCByRequestedTime = 0.15 * severeCalc(dur);
+  const pHBedsByRequestedTime = Math.trunc((severeCalc(dur) * 0.15) - (beds * 0.35));
   const pC4ICUByRequestedTime = (severeCalc(dur) * 0.05);
   const pC4VentilatorsByRequestedTime = (severeCalc(dur) * 0.02);
   const pDollarsInFlight = ((severeCalc(dur) * percentIncome * avgInc)) / timeEst(dur);
