a290fe16ada96d86df2c3f976a2911a0ed3525ef
TySod <36824700+TySod@users.noreply.github.com>
2020-04-14 03:45:36 +0100

    challenge 2d

--- src/estimator.js
+++ src/estimator.js
@@ -45,14 +45,14 @@ const covid19ImpactEstimator = (data) => {
       return pInfected * (2 ** Math.trunc((data.timeToElapse * 7) / 3));
     }
     if (cases === 'months') {
-      return pInfected * (2 ** Math.floor((data.timeToElapse * 30) / 3));
+      return pInfected * (2 ** Math.trunc((data.timeToElapse * 30) / 3));
     }
-    return pInfected * (2 ** Math.floor(data.timeToElapse / 3));
+    return pInfected * (2 ** Math.trunc(data.timeToElapse / 3));
   };
 
   const eRequestedTime = periodCheck(dur);
   const eSCasesByRequestedTime = (0.15 * periodCheck(dur));
-  const eHBedsByRequestedTime = Math.floor(0.35 * beds - (0.15 * periodCheck(dur)));
+  const eHBedsByRequestedTime = Math.trunc(0.65 * beds - (0.15 * periodCheck(dur)));
   const eC4ICUByRequestedTime = (periodCheck(dur) * 0.05);
   const eC4VentilatorsByRequestedTime = (periodCheck(dur) * 0.02);
   const eDollarsInFlight = ((periodCheck(dur) * percentIncome * avgInc)) / timeEst(dur);
@@ -69,7 +69,7 @@ const covid19ImpactEstimator = (data) => {
 
   const pRequestedTime = severeCalc(dur);
   const pSCByRequestedTime = severeCalc(dur);
-  const pHBedsByRequestedTime = Math.trunc(0.35 * beds - (severeCalc(dur) * 0.15));
+  const pHBedsByRequestedTime = Math.trunc(0.65 * beds - (severeCalc(dur) * 0.15));
   const pC4ICUByRequestedTime = (severeCalc(dur) * 0.05);
   const pC4VentilatorsByRequestedTime = (severeCalc(dur) * 0.02);
   const pDollarsInFlight = ((severeCalc(dur) * percentIncome * avgInc)) / timeEst(dur);
